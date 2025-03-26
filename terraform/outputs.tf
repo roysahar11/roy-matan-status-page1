92d3b4ca8312efdc7cc83b011deb89e9e0b50491
roysahar <roysahar11@gmail.com>
2025-03-26 16:47:31 +0200

    Updated rds.tf with superuser and a default db

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,8 @@ output "status_page_endpoint" {
   value = "http://${aws_ecs_service.status_page_nginx.network_configuration[0].assign_public_ip ? "localhost" : "private"}:80"
   description = "The endpoint to access Roy Matan Status Page"
 } 
+
+output "rds_endpoint" {
+  value       = aws_db_instance.production_rds.endpoint
+  description = "The DNS name of the RDS instance"
+}
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,13 +1,13 @@
-
-resource "aws_db_instance" "localstack_rds" {
-  identifier           = "my-localstack-db"
+resource "aws_db_instance" "production_rds" {
+  identifier           = "roymatan-status-page-production-rds-postgress"
   allocated_storage    = 20
   storage_type         = "gp2"
   engine              = "postgres"
   engine_version      = "12.7"
   instance_class      = "db.t3.micro"
-  username           = "admin"
-  password           = "password123"
+  username           = var.postgress_username
+  password           = var.postgress_password
+  db_name             = "statuspage"
   publicly_accessible = true
   skip_final_snapshot = true
 }
